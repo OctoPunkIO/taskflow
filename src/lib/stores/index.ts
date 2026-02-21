cfe2ffd60c3f112e38ce250c67ee614592ae14f9
DrOctoPunk <sales@octopunk.io>
2026-02-20 23:01:44 -0500

    Add stores index

---
+++ src/lib/stores/index.ts
@@ -0,0 +1,4 @@
+// Central store exports
+export { projectStore, currentProject, projectTasks } from './projects.js';
+export { taskCache, clearTaskCache } from './taskCache.js';
+export type { ProjectSubscription } from './types.js';
